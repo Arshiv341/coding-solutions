b4f08ed890ec31e560db67b9984ab2b96d78f588
Ritik Tiwari <drritik341@gmail.com>
2026-08-26 01:30:43 +0530

    ✅ TALLER - Rating 281 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-08-25

--- codechef/easy/TALLER---Rating-281/solution.java
+++ codechef/easy/TALLER---Rating-281/solution.java
@@ -7,6 +7,18 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int A=sc.nextInt();
+		    int B=sc.nextInt();
+		    if(A>B){
+		        System.out.println("A");
+		    }
+		    else{
+		        System.out.println("B");
+		    }
+		}
 
 	}
 }
