795b4b4b206c9130942fddbdc121a438d71a8adb
Ritik Tiwari <drritik341@gmail.com>
2026-09-01 20:22:26 +0530

    ✅ THREETOPICS - Rating 573 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-01

--- codechef/easy/THREETOPICS---Rating-573/solution.java
+++ codechef/easy/THREETOPICS---Rating-573/solution.java
@@ -8,11 +8,15 @@ class Codechef
 	{
 		// your code goes here
 		Scanner sc= new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = sc.nextInt();
-		int x = sc.nextInt();
-		if(a==x || b == x|| c==x){
+		int count =0;
+		int[] arr = new int[4];
+		for(int i =0; i<4;i++){
+		    arr[i]=sc.nextInt();
+		    if(arr[i]<4){
+		        count++;
+		    }
+		}
+		if(count>=3){
 		    System.out.println("YES");
 		}
 		else{
