90339450662dd94fe87a521bdb6c3e3344dafb6a
Ritik Tiwari <drritik341@gmail.com>
2026-09-02 03:43:21 +0530

    ✅ QUALIFY - Rating 593 | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-01

---
+++ codechef/easy/QUALIFY---Rating-593/solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new  Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int x = sc.nextInt();
+		    if(x%4==0){
+		        System.out.println("GOOD");
+		    }
+		    else{
+		        System.out.println("NOT GOOD");
+		    }
+		}
+
+	}
+}
