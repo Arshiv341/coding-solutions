840384d2b5eadd68347fae2b81a214b8974b70f7
Ritik Tiwari <drritik341@gmail.com>
2026-09-09 21:49:49 +0530

    ✅ EGGBUY | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/EGGBUY/solution.java
+++ codechef/medium/EGGBUY/solution.java
@@ -10,11 +10,5 @@ class Codechef
 	    int x= sc.nextInt();
 	    int y = sc.nextInt();
 	    int f = sc.nextInt();
-	    if(x*12<=y*12){
-	        System.out.println(x*12);
-	    }
-	    else{
-	        System.out.println(y*12 +f);
-	    }
 	}
 }
