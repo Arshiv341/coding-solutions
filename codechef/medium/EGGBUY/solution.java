7f34745ef177dca7c5cec5fa8a16adaa3a1feae5
Ritik Tiwari <drritik341@gmail.com>
2026-09-09 23:09:49 +0530

    ✅ EGGBUY | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-09

--- codechef/medium/EGGBUY/solution.java
+++ codechef/medium/EGGBUY/solution.java
@@ -6,15 +6,7 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-	    Scanner sc = new Scanner(System.in);
-	    int x= sc.nextInt();
-	    int y = sc.nextInt();
-	    int f = sc.nextInt();
-	    if(x*12<y*12){
-	        System.out.println(x*12);
-	    }
-	    else{
-	        System.out.println(y*12 +f);
-	    }
+	    
+
 	}
 }
